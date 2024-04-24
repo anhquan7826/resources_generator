12405445690bc45e5e9f8f352d172183b9c5fdfe
anhquan7826 <anhquan7826@gmail.com>
2024-04-24 09:06:08 +0700

    impr: root resource files sorting

--- lib/root_generator.dart
+++ lib/root_generator.dart
@@ -2,31 +2,32 @@ import 'dart:io';
 
 import 'package:path/path.dart';
 
-void generateRootResources(
-    {required String output, String? package}) {
+void generateRootResources({required String output, String? package}) {
   final directory = Directory(output);
   final buffer = StringBuffer();
   final files = directory
       .listSync()
       .map((e) {
-    if (e is File) {
-      if (extension(e.path) == '.dart' && basename(e.path) != 'resources.dart') {
-        return e;
-      }
-    }
-  })
+        if (e is File) {
+          if (extension(e.path) == '.dart' &&
+              basename(e.path) != 'resources.dart') {
+            return e;
+          }
+        }
+      })
       .whereType<File>()
-      .toList();
+      .toList()
+    ..sort(
+      (a, b) => basename(a.path).compareTo(basename(b.path)),
+    );
   for (final file in files) {
     buffer.writeln("part '${basename(file.path)}';");
   }
-  buffer.writeln(
-'''
+  buffer.writeln('''
 
 class R {
   R._();
-'''
-  );
+''');
   for (final file in files) {
     switch (basenameWithoutExtension(file.path)) {
       case 'string_resources':
